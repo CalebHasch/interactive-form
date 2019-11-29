a8ab88664687a6638a2f316b0c214e0983e0c7ec
Haschc <55303961+Haschc@users.noreply.github.com>
2019-11-28 19:23:58 -0700

    extra credit

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ const $total = $('<span></span>').text('Total = $');
 const $cost0 = $('<span></span>').text('0');
 const $payment = $('#payment');
 const $checkboxErrorMessage = $('<span></span>').text('You must pick one!').css({'color':'red', 'border':'2px solid red'});
+const $zipMessage =$('<span></span>').text('Must enter zip code').css({'color':'red', 'border':'2px solid red'});
+const $zipMessage2 = $('<span></span>').text('Must be 5 digits long').css({'color':'red', 'border':'2px solid red'});
 let cost = 0;
 
 //focus on name input
@@ -24,6 +26,7 @@ $title.on('change', function() {
 });
 
 //sets up the initial setup for theme and color selection
+$('#colors-js-puns').hide();
 $colorOptions.eq(0).text('Please select a T-Shirt theme');
 $colorOptions.each(function(index) {
     $colorOptions.attr('disabled', true);
@@ -54,6 +57,7 @@ $design.on('change', function() {
             $colorOptions.eq(i - 3).attr('hidden', true);
         }
     }
+    $('#colors-js-puns').show();
 })
 
 // creates a cost total and disables checkboxes/ creates message for time constrictions based off user checkbox selection
@@ -142,6 +146,11 @@ function emailValidation() {
     }
     return result; 
 }
+
+$('#mail').on('keyup', function() {
+    emailValidation();
+});
+
 function cardNumberValidation() {
     let result = /^\d{13}\d?\d?\d?$/.test($('#cc-num').val());
     let message = $('<span></span>').text('Invalid Input!').css({'color':'red', 'border':'2px solid red'});;
@@ -155,14 +164,15 @@ function cardNumberValidation() {
 }
 
 function zipCodeValidation() {
-    let result = /^\d{5}$/.test($('#zip').val());
-    let message = $('<span></span>').text('Invalid Input!').css({'color':'red', 'border':'2px solid red'});;
-    if ($('#zip').prev().text() === 'Invalid Input!') {
-        $('#zip').prev().remove();
-    }
+    let result = /.+/.test($('#zip').val());
+    let result2 = /^\d{5}$/.test($('#zip').val());
+    $zipMessage.remove();
+    $zipMessage2.remove();
     if (!result) {
-        $('#zip').before(message);
-    } 
+        $('#zip').before($zipMessage);
+    } else if (!result2) {
+        $('#zip').before($zipMessage2);
+    }
     return result;
 }
 
